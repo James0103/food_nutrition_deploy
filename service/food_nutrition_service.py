f5b5b233b926723e7783dfcb7451257a6c73aff0
Jameshhjung <42224985+James0103@users.noreply.github.com>
2025-08-27 16:04:45 +0900

    250827_ui_update_3

--- service/food_nutrition_service.py
+++ service/food_nutrition_service.py
@@ -1,6 +1,7 @@
 # 필요한 라이브러리들을 가져옵니다.
 import sys
 import os
+import re
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 # import src.db.database as DB
 from db import database as DB
